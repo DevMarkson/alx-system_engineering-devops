a067eca22df8b99a09571329eb7438bdb1175f3c
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-03-25 00:40:18 +0100

    a retouch

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-# Using Puppet, create a file in /tmp:
-
+# Creates a file
 file { '/tmp/school':
-    ensure  => present,
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
