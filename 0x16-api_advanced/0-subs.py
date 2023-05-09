9ef396e7d987253d2adf94bd8dbb64936da89cef
Markson17 <marksonfrosh@gmail.com>
2023-05-09 19:32:00 +0100

    file executable

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,10 @@ import requests
 
 def number_of_subscribers(sub):
     try:
-        res = requests.get('https://www.reddit.com/r/{}/about.json'.
-                            format(sub),
-                            headers={'User-Agent': 'custom'},
-                            allow_redirects=False)
-        return res.json().get('data').get('subscribers')
+        res = requests.get('https://www.reddit.com/r/{}/about.json'.    # URL to query
+                            format(sub),    # Subreddit name
+                            headers={'User-Agent': 'custom'},   # Custom user agent
+                            allow_redirects=False)  # Prevents redirection  
+        return res.json().get('data').get('subscribers')    # Returns the number of subscribers
     except:
-        return 0
+        return 0  # Returns 0 if an error occurred
