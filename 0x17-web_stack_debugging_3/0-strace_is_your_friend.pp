055ba66a38ccadb822cfff6301b765af1eb85730
Markson17 <marksonfrosh@gmail.com>
2023-05-11 02:18:27 +0100

    another commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# This code replaces the old value "phpp" with the new value "php" in the wp-settings.php file of the WordPress installation located at /var/www/html/. It does this using the sed command, which is run as a sudo command with the necessary permissions. By using clearer variable names, this code becomes easier to read and maintain in the long run.
+
 exec { 'replace-wordpress-phpp-with-php':
   environment => ['DIRECTORY=/var/www/html/wp-settings.php',
                   'OLD_VALUE=phpp',
