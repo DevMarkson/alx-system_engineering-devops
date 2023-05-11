677197927ebdd376a693bed5a009bb22577eb6af
Markson17 <marksonfrosh@gmail.com>
2023-05-11 02:14:37 +0100

    made cool changes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,8 @@
-# This code fixes a typo in the wp-settings.php file by using the 'sed' command to replace the incorrect filename 'class-wp-locale.phpp' with the correct filename 'class-wp-locale.php'. The file path is specified using the variable $file_path, and the corrected filename is stored in the variable $corrected_filename. The 'exec' function is used to execute the 'sed' command, which searches and replaces the incorrect filename in the specified file. This code assumes that the 'sed' command is available on the system and that the file path is correct.
-
-$file_path = '/var/www/html/wp-settings.php';
-$corrected_filename = 'class-wp-locale.php';
-
-exec("sed -i -e 's/class-wp-locale.phpp/{$corrected_filename}/g' {$file_path}");
+exec { 'replace-wordpress-phpp-with-php':
+  environment => ['DIRECTORY=/var/www/html/wp-settings.php',
+                  'OLD_VALUE=phpp',
+                  'NEW_VALUE=php'],
+  command     => 'sudo sed -i "s/$OLD_VALUE/$NEW_VALUE/" $DIRECTORY',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
+}
