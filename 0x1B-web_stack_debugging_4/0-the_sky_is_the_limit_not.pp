2b90296224012fd33ecc926623953e61d79fc1e3
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-05-19 01:01:44 +0100

    another commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,12 @@
 # code to fix nginx to accept and serve more requests
+
 exec { 'fix--for-nginx':
-  path    => '/usr/local/bin/:/bin/', 
-  command => 'sed -i "s/15/4096/" /etc/default/nginx'
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 } ->
 
-# code to restart Nginx
+# code to Restart Nginx
 exec { 'nginx-restart':
-  path    => '/etc/init.d/',
-  command => 'nginx restart'
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
