7993df3be4402ab9ee0b3beb6482b887bd6ee8c9
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-05-19 01:04:29 +0100

    web stack debugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
-# Modify OS Security Configuration for Seamless Login and File Access
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without an error message.
 
 exec {'OS security config':
-command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-path => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
